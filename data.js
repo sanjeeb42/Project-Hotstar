45265d4688343f2f3ec369c87cfe9c30fdbd0b00
sanjeeb42 <88538586+sanjeeb42@users.noreply.github.com>
2022-06-24 00:33:48 +0530

    Update data.js

--- data.js
+++ data.js
@@ -2,26 +2,26 @@ let movies = [
     {
         name: 'falcon and the winter soldier',
         des:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'Images/slider 2.png'
+        image: './Images/slider 2.png'
     },
     {
         name: 'loki',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'Images/slider 1.jpg'
+        image: './Images/slider 1.jpg'
     },
     {
         name: 'wanda vision',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'Images/slider 3.PNG'
+        image: './Images/slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'Images/slider 4.PNG'
+        image: './Images/slider 4.PNG'
     },
     {
         name: 'luca',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'Images/slider 5.PNG'
+        image: './Images/slider 5.PNG'
     }
 ]
